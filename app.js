0507f22a4813249546991b8760150a2860df0cf4
Ade3164 <Adebayoidris051@gmail.com>
2023-09-09 00:44:52 +0100

    HNG updated

--- app.js
+++ app.js
@@ -24,5 +24,5 @@ app.get('/api', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}/api?slack_name=Idris_Adebayo&track=Backend`);
 });
