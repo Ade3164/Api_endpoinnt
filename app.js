eb0ac92333e9304ecbf0706e8be36ec32dd12a73
Ade3164 <Adebayoidris051@gmail.com>
2023-09-08 14:14:00 +0100

    Stage_one_HNG_intern

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can choose any port you prefer
+const port = 3000; 
 
 app.get('/api', (req, res) => {
-  // Retrieve query parameters
-  const slackName = 'Idris_Adebayo'; // Set the slack_name parameter here
-  const track = 'Backend'; // Set the track parameter here
-
-  // Get the current day of the week
+  const slackName = 'Idris_Adebayo'; 
+  const track = 'Backend'; 
   const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
-
-  // Get the current UTC time with a +/-2 minute window
   const now = new Date();
   const currentUtcTime = new Date(now.getTime() - now.getTimezoneOffset() * 60000).toISOString();
-
-  // Hardcoded GitHub URLs
-  const githubFileUrl = '';
+  const githubFileUrl = 'https://github.com/Ade3164/Api_endpoinnt/blob/main/app.js';
   const githubRepoUrl = 'https://github.com/Ade3164/Api_endpoinnt.git';
-
-  // Create JSON response
   const response = {
     slack_name: slackName,
     current_day: currentDay,
